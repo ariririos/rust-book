08aa51541426e28723c8ea2aa209aaba2deb8ac2
Ari Rios <aririos0719@gmail.com>
2025-09-26 01:58:40 -0400

    Ch 16

---
+++ threads/src/main.rs
@@ -0,0 +1,23 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {v:?}");
+        for i in 1..10 {
+            println!("number {i} from spawned thread");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("number {i} from main thread");
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    handle.join().unwrap();
+
+}
